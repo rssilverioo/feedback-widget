c6d68199ac40929c68d1c235c0bd900a0290bc7e
Rodrigo Silverio <rodrigo.s.silverio@outlook.comgit config --global user.email rodrigo.s.silverio@outlook.com>
2024-09-06 22:30:49 -0700

    adicionando feedbackRestartRequested

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -38,6 +38,7 @@ export function WidgetForm() {
   const [feedbackSent, setFeedbackSent] = useState(false)
 
   function handleRestartFeedback() {
+    setFeedbackSent(false)
     setFeedbackType(null)
   }
 
@@ -45,7 +46,7 @@ export function WidgetForm() {
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
       {feedbackSent ? (
-        <FeedbackSuccessStep />
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
       ) : (
         <>
           {!feedbackType ? (
